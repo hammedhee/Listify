e45ff9e9b0468e0f7f4aae5621fd2d374e2c0899
muhammedsha@123 <muhammednk536@gmail.com>
2024-12-02 18:12:21 +0530

    new

--- lib/view/fruitslistpage.dart
+++ lib/view/fruitslistpage.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:listify/function/groseryListFunctions.dart';
 
-class Fruitslistpage extends StatelessWidget {
+List<String> options = ["inCompleted", "completed"];
+
+// ignore: must_be_immutable
+class Fruitslistpage extends StatefulWidget {
   String? catogary;
   Fruitslistpage({
     super.key,
@@ -9,17 +13,25 @@ class Fruitslistpage extends StatelessWidget {
   });
 
   @override
+  State<Fruitslistpage> createState() => _FruitslistpageState();
+}
+
+class _FruitslistpageState extends State<Fruitslistpage> {
+  String? currntOption;
+
+  @override
   Widget build(BuildContext context) {
     getAllgroseryData();
     return Scaffold(
         appBar: AppBar(
           leading: Icon(Icons.line_style),
+          title: Text('Your List Of Fruits'),
         ),
         body: ValueListenableBuilder(
           valueListenable: groseryListNotifyr,
           builder: (context, value, child) {
             final cato =
-                value.where((cat) => cat.catocary == catogary).toList();
+                value.where((cat) => cat.catocary == widget.catogary).toList();
 
             if (cato.isEmpty) {
               return Center(
@@ -30,11 +42,83 @@ class Fruitslistpage extends StatelessWidget {
               itemCount: cato.length,
               itemBuilder: (context, index) {
                 final data = cato[index];
-                return ListTile(
-                  onTap: () {
-                    deleteGroseryData(index);
-                  },
-                  title: Text(data.groceryName!),
+                return SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(18.0),
+                          child: ExpansionTile(
+                            backgroundColor:
+                                const Color.fromARGB(255, 255, 244, 203),
+                            title: Center(
+                                child: Text(
+                              data.groceryName!,
+                              style: TextStyle(fontSize: 18),
+                            )),
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(18.0),
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(data.quantity!),
+                                        Gap(20),
+                                        IconButton(
+                                            onPressed: () {
+                                              deleteGroseryData(index);
+                                            },
+                                            icon: Icon(Icons.delete))
+                                      ],
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Text('completed'),
+                                            Radio(
+                                                value: options[0],
+                                                groupValue: currntOption,
+                                                onChanged: (value) {
+                                                  setState(() {
+                                                    currntOption =
+                                                        value.toString();
+                                                  });
+                                                }),
+                                          ],
+                                        ),
+                                        Row(
+                                          children: [
+                                            Text('Not Completed'),
+                                            Radio(
+                                                value: options[1],
+                                                groupValue: currntOption,
+                                                onChanged: (value) {
+                                                  setState(() {
+                                                    currntOption =
+                                                        value.toString();
+                                                  });
+                                                }),
+                                          ],
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Gap(10)
+                      ],
+                    ),
+                  ),
                 );
               },
             );
